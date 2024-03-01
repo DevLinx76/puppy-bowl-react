536436cc22aa68596ea2e4997d5a28e94f6ac0f4
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2024-02-29 19:16:50 -0500

    modified

--- src/API/index.js
+++ src/API/index.js
@@ -1,16 +1,31 @@
 // src/API/index.js
 
 
-
 const API_URL = 'https://fsa-puppy-bowl.herokuapp.com/api/2302-ACC-CT-WEB-PT-B/players';
 
-export function fetchPlayers() {
+export const fetchPlayers = async () => {
   try {
     const response = await fetch(API_URL);
-    const data = await response.json();
-    return data;
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const result = await response.json();
+    return result;
   } catch (error) {
     console.error('Error fetching players', error);
+  }  
+};
+
+export const fetchPlayer = async (id) => {
+  try {
+    const response = await fetch(`${API_URL}/${id}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error fetching player', error);
   }
-    
 }
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AllPlayer from './Components/AllPlayer'
+import SinglePlayer from './Components/SinglePlayer'
+import NewPlayerForm from './Components/NewPlayerForm'
+import NavBar from './Components/NavBar'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://fsa-puppy-bowl.herokuapp.com/api/2302-ACC-CT-WEB-PT-B/players" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<AllPlayer />} />
+        <Route path="/:id" element={<SinglePlayer />} />
+        <Route path="/new" element={<NewPlayerForm />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
