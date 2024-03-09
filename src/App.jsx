91cc2c13645add49a157ba1650dc1ad0f04d9345
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2024-03-09 00:42:21 -0500

    edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
+// App.jsx
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AllPlayers from './Components/AllPlayers'; // Correctly imported
+import { Routes, Route } from 'react-router-dom';
+import AllPlayers from './Components/AllPlayers'; 
 import SinglePlayer from './Components/SinglePlayer';
 import NewPlayerForm from './Components/NewPlayerForm';
 import NavBar from './Components/NavBar';
 import './App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
+  return ( 
+    <>      
       <NavBar />
       <Routes>
-        <Route path="/" element={<AllPlayers />} 
+        <Route path="/" element={<AllPlayers />} />
         <Route path="/:id" element={<SinglePlayer />} />
-        <Route path="/new" element={<NewPlayerForm />} />
+        <Route path="/new" element={<NewPlayerForm />} />      
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
